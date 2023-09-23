145ea29645312cdec2b58eb6f207a02f3cb14129
Ngoc Thanh <ngocthanh@wearebloom.com.au>
2023-09-23 14:11:14 +0700

    FIX: update UI

--- pages/success/index.jsx
+++ pages/success/index.jsx
@@ -3,7 +3,7 @@ import '../../app/globals.css';
 export default function SuccessPage() {
   return (
     <div className="text-center w-full text-xl my-8 ">
-      <h1>Thanks for you request :)</h1>
+      <h1>Thank you for your request :)</h1>
     </div>
   );
 }
